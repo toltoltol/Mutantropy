df4356bca38aa42ed2ecd876fbbd9635448bb20a
Harrison Doran <harrydoran@pm.me>
2025-01-23 20:48:20 +1100

    Add GameMaster for movement speed multiplier

--- Assets/Scripts/EnemyPeek.cs
+++ Assets/Scripts/EnemyPeek.cs
@@ -16,6 +16,7 @@ public class EnemyPeek : MonoBehaviour
 
     private EnemyAttributes enemyAttributes;
     public bool isPeeking = false;    // Whether the enemy is currently peeking
+    private EnemyAttributes enemyAttributes;
 
     private void Start()
     {
