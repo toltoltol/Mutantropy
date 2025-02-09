550a261e84d8e40304a69fe6682c4ae52475572d
Max Wallace <walma865@student.otago.ac.nz>
2025-02-10 12:28:45 +1300

    Added skip cutscene function

---
+++ Assets/Scripts/UI/SkipCutscene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipCutscene : MonoBehaviour
+{
+    public GameObject cutsceneCanvas;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Skip();
+        }
+
+    }
+
+    void Skip() {
+        if (cutsceneCanvas != null) {
+            //cutsceneCanvas.SetActive(false);
+            SceneManager.LoadScene("Room1");
+        }
+    }
+}
