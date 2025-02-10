1a58b4e436839406ba89e96afc01e8e96e1e109e
mahja805 <mahja805@student.otago.ac.nz>
2025-02-10 14:48:44 +1300

    Added Prefabs for gate collision dectection, added the prefabs to all rooms. Added Scripts for Camera Control & Triggers for room transistions.

---
+++ Assets/Scripts/Transition/CamController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public Transform player;
+    public float smoothSpeed;
+
+    private Vector3 targetPos, newPos;
+
+    public Vector3 minPos, maxPos;
+
+    void LateUpdate()
+    {
+        if (transform.position != player.position)
+        {
+            targetPos = player.position;
+
+            Vector3 camBoundaryPos = new Vector3(
+                Mathf.Clamp(targetPos.x, minPos.x, maxPos.x),
+                Mathf.Clamp(targetPos.y, minPos.y, maxPos.y),
+                Mathf.Clamp(targetPos.z, minPos.z, maxPos.z));
+
+            newPos = Vector3.Lerp(transform.position, camBoundaryPos, smoothSpeed);
+            transform.position = newPos;
+        }
+    }
+}
