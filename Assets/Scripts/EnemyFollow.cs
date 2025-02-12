4a501c0891f40b779f6685d9e22bdc56e475d055
Max Wallace <walma865@student.otago.ac.nz>
2025-02-12 18:32:49 +1300

    Make enemies face direction of movement and update enemy projectile

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -4,12 +4,15 @@ public class EnemyFollow : MonoBehaviour
 {
     public Transform player;       // Reference to the player's Transform
     public float stopDistance = 1f; // Distance to stop from the player
+    public bool flip;
 
     private EnemyAttributes enemyAttributes;
+    
 
     private void Awake()
     {
         enemyAttributes = GetComponent<EnemyAttributes>();
+
         if (enemyAttributes == null)
         {
             Debug.LogError("EnemyAttributes component is missing on this enemy.");
@@ -23,6 +26,7 @@ public class EnemyFollow : MonoBehaviour
         {
             return;
         }
+        
 
         // Calculate the distance between the enemy and the player
         float distance = Vector3.Distance(transform.position, player.position);
@@ -35,6 +39,20 @@ public class EnemyFollow : MonoBehaviour
 
             // Move the enemy in the direction of the player
             transform.position += direction * enemyAttributes.moveSpeed * GameMaster.GetEnemyMoveSpeedMultiplier() * Time.deltaTime;
+           
         }
+
+        Vector3 scale = transform.localScale;
+
+        if (player.transform.position.x > transform.position.x)
+        {
+            scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 1);
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1);
+        }
+
+        transform.localScale = scale;
     }
 }
