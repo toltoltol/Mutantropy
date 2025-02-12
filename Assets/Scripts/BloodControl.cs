4879f4320b874f7aa95258a34bc0002e0862f94a
Harrison Doran <harrydoran@pm.me>
2025-02-12 14:20:01 +1100

    Flip player blood overlay when turning

--- Assets/Scripts/BloodControl.cs
+++ Assets/Scripts/BloodControl.cs
@@ -8,17 +8,21 @@ public class BloodControl : MonoBehaviour
 {
     // Start is called before the first frame update
     public PlayerAttributes playerAttributes;
+    private SpriteRenderer _playerSpriteRenderer;
     private SpriteRenderer _spriteRenderer;
-    
+
     // Update is called once per frame
     private void Start()
     {
         if (playerAttributes == null)
         {
             gameObject.SetActive(false);  // Hide
+            return;
         }
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _spriteRenderer.enabled = false;
+
+        _playerSpriteRenderer = playerAttributes.GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -28,6 +32,12 @@ public class BloodControl : MonoBehaviour
             _spriteRenderer.enabled = true;
         } else {
             _spriteRenderer.enabled = false;
+        }
+
+        // Mirror the player sprite flipX
+        if (_playerSpriteRenderer != null) // Important null check!
+        {
+            _spriteRenderer.flipX = _playerSpriteRenderer.flipX;
         }
     }
 }
